cde517c0027055b18ba62d1d1531b1abd4387469
gurupreethsingh <igurupreeth@gmail.com>
2024-02-04 02:49:38 +0530

    arrays topics added

---
+++ jsfiles/07_arrays.js
@@ -0,0 +1,25 @@
+// array creation and printing the arrays. 
+
+ var a =  [11,22,33,44,55];
+
+//  console.log(a);
+//  console.log(a[0]);
+//  console.log(a[1]);
+//  console.log(a[2]);
+//  console.log(a[3]);
+//  console.log(a[4]);
+//  console.log(a[5]); // index out of boundary. // undefined
+
+ // priting the array using for loop/. 
+
+// for(let i = 0; i<a.length; i++)
+// {
+//     console.log(a[i]);
+// }
+
+
+// printing array in revers using for loop. 
+for(let i = a.length-1; i>=0; i--)
+{
+    console.log(a[i]);
+}
